7488380f4eb9b841918a6a9ced157a792bee7cba
rohanj48 <rohanjosepharun@gmail.com>
2025-12-03 21:42:35 +0530

    Frontend initial Setup

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+
+    return (
+        <div className='text-center'>
+            Habbit-Tracker
+        </div>
+    )
+}
+
+export default App
